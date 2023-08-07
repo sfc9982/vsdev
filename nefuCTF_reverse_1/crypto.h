5993c4369d3c1ac16e3f8d1b9d77abbd5f499c82
sfc9982 <26681591+sfc9982@users.noreply.github.com>
2023-08-07 15:51:28 +0800

    Add: Custom Key for TEA
    Signed-off-by: sfc9982 <26681591+sfc9982@users.noreply.github.com>

--- nefuCTF_reverse_1/crypto.h
+++ nefuCTF_reverse_1/crypto.h
@@ -5,3 +5,6 @@
 void        hexdumpStr(std::string str);
 std::string encryptStr(std::string str);
 std::string decryptStr(std::string str);
+
+extern bool        useCustomKey;
+extern std::string CustomKey;
