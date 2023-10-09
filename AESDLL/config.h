97b54ef3a9626f3c72180c86ab39c6b5495618f1
Jackson Chen <26681591+sfc9982@users.noreply.github.com>
2023-10-09 13:36:13 +0800

    add: AES

---
+++ AESDLL/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Enable ECB, CTR and CBC mode. Note this can be done before including aes.h or at compile-time.
+// E.g. with GCC by using the -D flag: gcc -c aes.c -DCBC=0 -DCTR=1 -DECB=1
+#define CBC 1
+#define CTR 1
+#define ECB 1
+#define AES256 1
