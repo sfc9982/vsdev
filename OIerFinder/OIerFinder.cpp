3af526fdca265f3758c1c00173c6ff9c1cb8f270
Jackson Chen <sfc9982@googles.plus>
2023-11-10 13:27:06 +0800

    perf: OIer finder

--- OIerFinder/OIerFinder.cpp
+++ OIerFinder/OIerFinder.cpp
@@ -20,35 +20,36 @@
 constexpr unsigned int CSV_COLUMN   = 9;
 constexpr int          CURRENT_YEAR = 2023;
 
-std::string sliceName(std::string);
+std::string sliceName(const std::string &);
 
 int main() {
     io::CSVReader<CSV_COLUMN> in("oierdb.csv");
+    std::set<std::string>     oiers;
 
-    int         uid;
-    std::string initials;
-    std::string name;
-    int         gender;
-    int         enroll_middle;
-    double      oierdb_score;
-    double      ccf_score;
-    int         ccf_level;
-    std::string compressed_records;
+    {
+        int         uid;
+        std::string initials;
+        std::string name;
+        int         gender;
+        int         enroll_middle;
+        double      oierdb_score;
+        double      ccf_score;
+        int         ccf_level;
+        std::string compressed_records;
 
-    std::set<std::string> oiers;
 
-    auto start = std::chrono::steady_clock::now();
-    while (in.read_row(uid, initials, name, gender, enroll_middle, oierdb_score, ccf_score, ccf_level, compressed_records)) {
-        if (enroll_middle + 6 == CURRENT_YEAR) {
-            oiers.insert(name);
+        auto start = std::chrono::steady_clock::now();
+        while (in.read_row(uid, initials, name, gender, enroll_middle, oierdb_score, ccf_score, ccf_level, compressed_records)) {
+            if (enroll_middle + 6 == CURRENT_YEAR) {
+                oiers.insert(name);
+            }
         }
+        auto end     = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        fmt::println("{} records loaded.", oiers.size());
+        fmt::println("Time cost: {}", elapsed);
+        fmt::println("\nOIers: ");
     }
-    auto end     = std::chrono::steady_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    fmt::println("{} records loaded.", oiers.size());
-    fmt::println("Time cost: {}", elapsed);
-    fmt::println("\nOIers: ");
-
 
     Json::Value   newbie;
     std::ifstream newbie_file("newbie.json", std::ifstream::binary);
@@ -73,17 +74,17 @@ int main() {
     return 0;
 }
 
-std::string sliceName(std::string name) {
-    UErrorCode         status = U_ZERO_ERROR;
-    icu::UnicodeString ucs    = icu::UnicodeString::fromUTF8(icu::StringPiece(name.c_str()));
-    icu::RegexMatcher  matcher("^.*( |-|–|—|－|人工智能|大数据|类|计算机)", 0, status);
+std::string sliceName(const std::string &name) {
+    UErrorCode               status = U_ZERO_ERROR;
+    const icu::UnicodeString ucs    = icu::UnicodeString::fromUTF8(icu::StringPiece(name.c_str()));
+    icu::RegexMatcher        matcher("^.*( |-|–|—|－|人工智能|大数据|类|计算机)", 0, status);
     if (U_FAILURE(status)) {
         fmt::println("Regex init failed.");
     }
 
     matcher.reset(ucs);
-    icu::UnicodeString result;
-    icu::UnicodeString replacement("");
+    icu::UnicodeString       result;
+    const icu::UnicodeString replacement("");
 
     while (matcher.find(status) && U_SUCCESS(status)) {
         matcher.appendReplacement(result, replacement, status);
